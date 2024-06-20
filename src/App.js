0e711a70e98861954f5f7c879c98b4c5ff425565
Gajendra Nagpure <nagpuregajendra704@gmail.com>
2024-06-20 11:49:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import Random from './Component/Random';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Gajendra N
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </header>
+      <Random/>
+    </div>
+  );
+}
+
+export default App;
